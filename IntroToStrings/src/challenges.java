b0407bf4b96020c1a52ca9903d4fed254969d6c7
Blake Good <Blake Good@DESKTOP-44EP6HM.hsd1.co.comcast.net>
2018-08-29 21:10:08 -0600

    Still having some trouble. Making some progress

--- IntroToStrings/src/challenges.java
+++ IntroToStrings/src/challenges.java
@@ -1,21 +1,33 @@
-
+import java.util.Scanner;
 public class challenges
 	{
 
 		public static void main(String[] args)
 			{
-				challengeOne();
+				//challengeOne();
 				challengeTwo();
 
 			}
 		
 		public static void challengeOne()
 		{
-			Scanner
+			Scanner shoutingWords = new Scanner(System.in);
+			System.out.println("Please shout something using ALL CAPS!");
+			String shouting = new String (shoutingWords.nextLine());
+			shouting.toLowerCase();
+			System.out.println(shouting.toLowerCase());
 		}
 
 		public static void challengeTwo()
 		{
+			System.out.println("Please type something");
+			Scanner userInput = new Scanner(System.in);
+			String input = userInput.nextLine();
+			for (int i = 0; i <= input.length(); i++)
+			{
+				System.out.println(input.substring(i));
+			}
+			
 			
 		}
 		
